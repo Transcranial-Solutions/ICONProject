5ed807fb9077707353a819f6e76d0bbdc8cb1976
Ted Skyvell <tskyvell@gmail.com>
2020-06-02 16:13:00 +0200

    Wrote report_progress function.

--- transaction_extractor/transaction_extractor.py
+++ transaction_extractor/transaction_extractor.py
@@ -3,11 +3,14 @@ from blockchain import Block
 from blockchain import Transaction
 import plyvel
 import signal
+import time
+from threading import Timer
+import datetime
 
         
 
 def main():
-
+    # Handle command line arguments.
     parser = argparse.ArgumentParser(description="Tool for extracting transactions from " 
                                                   "ICON blockchain using filter criterias. "
                                                   "Transactions of different filtercriteria will "
@@ -25,17 +28,27 @@ def main():
                         help = 'Output folder')
     parser.add_argument('--syncronize', action = 'store_true',
                         help = "Syncronize previously written csv files up to current block height")
-    
-    args = parser.parse_args()
-    print(args)
-    
+     args = parser.parse_args()
 
+    
+    # Keep track of and report progress  Idea --> maybe make a class to handle this?
+    start_time = time.time()
+    block_counter = 0
+    transaction_counter = 0
+    timer = Time(60, report_progress)
+
+    # Keyword arguments to be passed to filtermethod in blockclass.
     filter_criteria = {}
     for criteria in args.filter:
         filter_criteria[criteria] = True
+   
+   
+    # Create csv files --> list of fileobjects?
     ## TODO
-    ## Create csv files --> list of fileobjects?
     
+    
+
+
     # Loop through all blocks and extract transactions according to filter criteria
     blocks = range(args.first-block, args.last-block + 1)
     counter = 0
@@ -50,8 +63,19 @@ def write_to_csv():
     pass
 
 def report_progress():
-    ## TODO
-    pass
+    runtime = time.time() - start_time
+    speed = (block_counter - lastblock) / 60
+    eta = ((args.last-block - block_counter) / speed)
+
+    print("Progress report")
+    print("---------------")
+    print(f"Runtime:       {datetime.timedelta(seconds(runtime)}")
+    print(f"Speed:         {speed} b/s")
+    print(f"Blocks:        {block_counter}/{args.last-block}  ")
+    print(f"Transactions:  {transaction_counter}  ")
+    print(f"Eta:           {datetime.timedelta(seconds(eta)}  ")
+    previous_block = block_counter
+    timer = Time(60, report_progress)
 
 def syncronize():
     ## TODO
