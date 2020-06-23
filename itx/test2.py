b5ae3151b8c41345b033dd1910e804ab948354bd
Ted <ted_skyvell@hotmail.com>
2020-06-23 19:09:10 +0200

    Deleted test.py and added test2.py

---
+++ itx/test2.py
@@ -0,0 +1,13 @@
+from blockchain import Block, Transaction
+import plyvel
+import json
+import sys
+
+def main():
+    print("First")
+    db = plyvel.DB("/home/ted/Iconnode/data/mainnet/.storage/db_31.208.165.65:7100_icon_dex", create_if_missing=False)
+    print(Block(0, db).find_last_block(db))
+
+
+if __name__=='__main__':
+    main()
