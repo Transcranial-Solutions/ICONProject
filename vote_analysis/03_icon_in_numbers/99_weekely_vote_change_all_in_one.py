89d87cc6b83b182506a29a0390aafee6350eb520
tonoplast <tonoplast.chloroplast@gmail.com>
2020-07-12 22:24:43 +1000

    Update 99_weekely_vote_change_all_in_one.py

--- vote_analysis/03_icon_in_numbers/99_weekely_vote_change_all_in_one.py
+++ vote_analysis/03_icon_in_numbers/99_weekely_vote_change_all_in_one.py
@@ -1050,11 +1050,8 @@ vote_stagnancy['pct_votes'] = vote_stagnancy['votes'] / vote_stagnancy['total_vo
 # vote stagnancy % by voters
 vote_stagnancy['total_voters'] = vote_stagnancy.groupby(measuring_interval)['voters'].transform('sum')
 vote_stagnancy['pct_voters'] = vote_stagnancy['voters'] / vote_stagnancy['total_voters']
-vote_stagnancy = vote_stagnancy[vote_stagnancy[measuring_interval] != '2019-34']
-
+vote_stagnancy = vote_stagnancy[vote_stagnancy[measuring_interval] != '2019-34']  # for order of the plot
 vote_stagnancy = vote_stagnancy.sort_values(by=['Stagnant', measuring_interval])
-
-
 
 # currPath = os.getcwd()
 # vote_stagnancy.to_csv(os.path.join(currPath, 'test.csv'), index=False)
