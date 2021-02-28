20120bbebbac681da4debbf684272fb8c718ac37
tonoplast <tonoplast.chloroplast@gmail.com>
2021-02-28 17:08:26 +1100

    Update 99_biweekely_vote_change_all_in_one.py

--- data_analysis/03_icon_in_numbers/99_biweekely_vote_change_all_in_one.py
+++ data_analysis/03_icon_in_numbers/99_biweekely_vote_change_all_in_one.py
@@ -51,7 +51,7 @@ measuring_interval = 'biweek' # // 'year' // 'month' // 'week' // "date" // "day
 
 alternating_biweek = 2 # starting from first week or 2nd week
 
-terms = ['2021-05 & 2021-06', '2021-03 & 2021-04']
+terms = ['2021-07 & 2021-08', '2021-05 & 2021-06']
 # weeks = ['2020-24', '2020-23']
 # months = ['2020-05', '2020-06']
 # years = ['2020']
@@ -695,8 +695,8 @@ def plot_vote_chage(ymin_mult=1.0, ymax_mult=1.4,
 
 # adjust these numbers to get proper plot
 plot_vote_chage(ymin_mult=1.0, ymax_mult=1.4, # these multiplier to change ylims
-                ymin_val=-3000000, ymax_val=11000000, ytick_scale=1000000, # these are actual ylims & tick interval20
-                voter_mult=0.85, voter_diff_mult=1.05, # voter change multiplier
+                ymin_val=-5000000, ymax_val=40000000, ytick_scale=5000000, # these are actual ylims & tick interval20
+                voter_mult=0.65, voter_diff_mult=1.05, # voter change multiplier
                 top10_1_mult=0.92, top10_2_mult=0.85, # where top 10 streak locates
                 topF_1_mult=0.55, topF_2_mult=0.47) # where top first locates
 
@@ -873,8 +873,8 @@ def plot_voter_chage(ymin_mult=1.1, ymax_mult=1.3,
 
 
 plot_voter_chage(ymin_mult=1.1, ymax_mult=1.3,
-                    ymin_val=-300, ymax_val=900, ytick_scale=100,
-                    first_time_voter_mult=0.95, new_voter_mult=1.20, ## change these
+                    ymin_val=-600, ymax_val=1600, ytick_scale=200,
+                    first_time_voter_mult=0.95, new_voter_mult=1.15, ## change these
                     top10_1_mult=0.90, top10_2_mult=0.83,
                     topF_1_mult=0.60, topF_2_mult=0.53)
 # saving
