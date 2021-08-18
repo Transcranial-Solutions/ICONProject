1e2665cab5707b841369fde21db9fb3b9ec8892d
Tono || Sung <tonoplast.chloroplast@gmail.com>
2021-08-18 10:04:00 +1000

    Update transaction_extraction_ctznode.py

--- data_analysis/08_transaction_data/transaction_extraction_ctznode.py
+++ data_analysis/08_transaction_data/transaction_extraction_ctznode.py
@@ -42,6 +42,8 @@ desired_width=320
 pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns',10)
 
+workers = 8
+
 currPath = os.getcwd()
 # currPath = "08_transaction_data"
 projectPath = os.path.join(currPath, "08_transaction_data")
@@ -169,10 +171,11 @@ else:
 # icon_service = IconService(HTTPProvider("https://ctz.solidwallet.io/api/v3"))
 # icon_service = IconService(HTTPProvider("http://localhost:9000", 3))
 # icon_service = IconService(HTTPProvider("https://ctz.solidwallet.io", 3, request_kwargs={"timeout": 60}))
+# icon_service = IconService(HTTPProvider("http://localhost:9000/api/v3", request_kwargs={"timeout": 60}))
 
 # localhost
 icon_service = IconService(HTTPProvider("http://127.0.0.1:9000/api/v3", request_kwargs={"timeout": 60}))
-# block = icon_service.get_block(1000) 
+# block = icon_service.get_block(38000000)
 
 
 ## Creating Wallet if does not exist (only done for the first time)
@@ -196,10 +199,13 @@ combined_df = pd.read_csv(os.path.join(dataPath, 'transaction_blocks_' + date_pr
 combined_df['date'] = pd.to_datetime(combined_df['block_createDate']).dt.strftime("%Y-%m-%d")
 df_of_interest = combined_df[combined_df['date'] == date_prev]
 
-block_of_interest = df_of_interest[df_of_interest['block_fee'] != 0]
-block_of_interest = block_of_interest['blockHeight']
+# block_of_interest = df_of_interest[df_of_interest['block_fee'] != 0]
+# block_of_interest = block_of_interest['blockHeight']
 
-# block = icon_service.get_block(1000)
+block_of_interest = df_of_interest['blockHeight']
+
+# block_of_interest = block_of_interest.iloc[0:10]
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Other way ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 def get_block_df(blockInfo):
@@ -217,40 +223,13 @@ def get_block_df(blockInfo):
     block_df = pd.DataFrame(data=combined_block)
     return block_df
 
-def get_tx_dfs(txHash):
-    tx = icon_service.get_transaction(txHash)
-    # try:
-    #     combined_tx = pd.DataFrame.from_dict(tx).reset_index()
-    #     if not combined_tx:
-    #         raise ValueError('empty string')
-    # except:
-    combined_tx = pd.json_normalize(tx)
-
-    tx_results = icon_service.get_transaction_result(txHash)
-    # try:
-    #     combined_tx_results = pd.DataFrame.from_dict(tx_results)
-    #     if not combined_tx_results:
-    #         raise ValueError('empty string')
-    # except:
-    combined_tx_results = pd.json_normalize(tx_results)
-
-    tx_df = combined_tx.drop(['signature','blockHeight','blockHash','to'], axis=1, errors='ignore').rename(columns={'timestamp':'tx_timestamp'})
-    tx_results_df = combined_tx_results.drop(['logsBloom', 'blockHeight', 'blockHash'], axis=1, errors='ignore')
-
-    tx_dfs = pd.merge(tx_df, tx_results_df, on=["txHash","txIndex"], how="left")
-
-    return tx_dfs
-
-# block_of_interest = block_of_interest.iloc[0:10]
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # collecting contact info using multithreading
 
 def run_block():
     start = time()
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
         myblock = list(tqdm(executor.map(get_block_df, block_of_interest), total=len(block_of_interest)))
 
     block_df = pd.concat(myblock).reset_index(drop=True)
@@ -275,15 +254,38 @@ block_df = pd.read_csv(os.path.join(dataPath, 'transactions_each_block_' + date_
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # getting hash only
 txHashes = block_df['txHash']
-# txHashes = txHashes.iloc[0:1000]
+# txHashes = txHashes.iloc[0:200]
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # collecting transaction info using multithreading
+def get_tx_dfs(txHash):
+    tx = icon_service.get_transaction(txHash)
+    # try:
+    #     combined_tx = pd.DataFrame.from_dict(tx).reset_index()
+    #     if not combined_tx:
+    #         raise ValueError('empty string')
+    # except:
+    combined_tx = pd.json_normalize(tx)
+
+    tx_results = icon_service.get_transaction_result(txHash)
+    # try:
+    #     combined_tx_results = pd.DataFrame.from_dict(tx_results)
+    #     if not combined_tx_results:
+    #         raise ValueError('empty string')
+    # except:
+    combined_tx_results = pd.json_normalize(tx_results)
+
+    tx_df = combined_tx.drop(['signature','blockHeight','blockHash','to'], axis=1, errors='ignore').rename(columns={'timestamp':'tx_timestamp'})
+    tx_results_df = combined_tx_results.drop(['logsBloom', 'blockHeight', 'blockHash'], axis=1, errors='ignore')
+
+    tx_dfs = pd.merge(tx_df, tx_results_df, on=["txHash","txIndex"], how="left")
+
+    return tx_dfs
 
 def run_tx():
     start = time()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
         mytx = list(tqdm(executor.map(get_tx_dfs, txHashes), total=len(txHashes)))
 
     tx_df = pd.concat(mytx).reset_index(drop=True)
@@ -303,11 +305,12 @@ def loop_to_icx(loop):
 def timestamp_to_date(df, timestamp, dateformat):
     return pd.to_datetime(df[timestamp] / 1000000, unit='s').dt.strftime(dateformat)
 
-def df_merge_all(tx_df=tx_df, block_df=block_df):
+def df_merge_all(tx_df, block_df):
     tx_all = pd.merge(block_df, tx_df, on=["txHash","tx_timestamp"], how="left")
     return(tx_all)
 
-def tx_data_cleaning_1(tx_all=tx_all):
+def tx_data_cleaning_1(tx_all):
+    tx_all = tx_all.rename(columns={'cumulativeStepUsed':'stepUsedCumulative'})
     tx_all['stepPrice'] = pd.to_numeric(tx_all['stepPrice'], errors='coerce').astype('Int64').map(loop_to_icx).fillna(0)
     tx_all['stepUsed'] = pd.to_numeric(tx_all['stepUsed'], errors='coerce').astype('Int64').fillna(0)
     tx_all['tx_fees'] = tx_all['stepUsed'] * tx_all['stepPrice']
@@ -315,6 +318,8 @@ def tx_data_cleaning_1(tx_all=tx_all):
     tx_all['block_time'] = timestamp_to_date(tx_all, 'block_timestamp', '%H:%M:%S')
     tx_all['tx_date'] = timestamp_to_date(tx_all, 'tx_timestamp', '%Y-%m-%d')
     tx_all['tx_time'] = timestamp_to_date(tx_all, 'tx_timestamp', '%H:%M:%S')
+
+    tx_all['value'] = loop_to_icx(tx_all['value'])
     return tx_all
 
 def get_list(df, strings):
@@ -327,9 +332,9 @@ def remove_list(cols, strings):
     for index in sorted(delete_list, reverse=True):
         del cols[index]
 
-def tx_data_cleaning_2(tx_all=tx_all):
+def tx_data_cleaning_2(tx_all):
     # exploding event logs
-    tx_all = tx_all.explode('eventLogs').reset_index(drop=True)
+    tx_all = tx_all.explode('eventLogs').reset_index(drop=True).sort_index(axis=1)
 
     # separating event logs by those that have NaN
     nonans = tx_all[tx_all['eventLogs'].notnull()]
@@ -347,7 +352,7 @@ def tx_data_cleaning_2(tx_all=tx_all):
     df1 = df1.rename(columns={"eventLogs_indexed":"eventLogs.indexed","eventLogs_data":"eventLogs.data"})
 
     df = df1.append(nans)
-    df = df.drop(columns=["eventLogs","data"])
+    df = df.drop(columns=["eventLogs","data"], axis=1, errors='ignore')
 
     # shifting data. and eventLogs. to the end
     cols = list(df.columns.values)  # Make a list of all of the columns in the df
@@ -357,15 +362,13 @@ def tx_data_cleaning_2(tx_all=tx_all):
     remove_list(cols, strings="data.")
     remove_list(cols, strings="eventLogs.")
 
-    df = df[cols + data_list + eventlog_list]  # Create new dataframe with columns in the order you want
+    df = df[cols + eventlog_list + data_list]  # Create new dataframe with columns in the order you want
 
     # shifting columns (from - to) together
     cols = list(df.columns.values)
-
     to_loc = df.columns.get_loc("to")
     from_loc = df.columns.get_loc("from")
-
-    df = df[cols[0:to_loc] + [cols[from_loc]] + cols[to_loc:from_loc] + cols[from_loc+1:]]
+    df = df[cols[0:from_loc+1] + [cols[to_loc]] + cols[from_loc+1:to_loc] + cols[to_loc+1:]]
 
     df = df.sort_values(by=['blockHeight','txIndex','tx_timestamp','txHash'])
 
@@ -374,12 +377,27 @@ def tx_data_cleaning_2(tx_all=tx_all):
 
 def final_output():
     tx_all = df_merge_all(tx_df=tx_df, block_df=block_df)
+    # tx_all = tx_all[tx_all['to'].notnull()]
     tx_all = tx_data_cleaning_1(tx_all=tx_all)
     tx_all = tx_data_cleaning_2(tx_all=tx_all)
     final_tx_df = pd.merge(df_of_interest, tx_all, on=['blockHeight'], how='left')
     return final_tx_df
 
 final_tx_df = final_output()
+
+
+
+
+# df['count'] = df['eventLogs.indexed'].str.count("Address")
+
+df['intTxCount'] = np.where(df['eventLogs.indexed'].str.contains('Address,Address,int', na=False), 1, 0)
+df['intEvtCount'] = np.where(df['eventLogs.data'].notnull(), 1, 0)
+
+
+test = df.groupby('txHash').agg('sum')[['intTxCount','intEvtCount']].reset_index()
+
+
+
 
 final_tx_df.to_csv(os.path.join(dataPath, 'tx_final_' + date_prev + '.csv'), index=False)
 
