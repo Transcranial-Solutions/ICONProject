83a67af862c099883f1c97bbdd83caaca23c21cf
Tono || Sung <tonoplast.chloroplast@gmail.com>
2021-12-19 00:44:02 +1100

    data update

--- data_analysis/daily_block_extraction.bat
+++ data_analysis/daily_block_extraction.bat
@@ -1,4 +1,2 @@
-wsl.exe ~/IconProject/.analysis/bin/python ~/IconProject/transaction_data/block_extraction.py
-wsl.exe ~/IconProject/.analysis/bin/python ~/IconProject/transaction_data/transaction_extraction_ctznode.py
-wsl.exe ~/IconProject/.analysis/bin/python ~/IconProject/transaction_data/transaction_analysis_ctznode.py
-wsl.exe ~/IconProject/.analysis/bin/python ~/IconProject/transaction_data/transaction_analysis_ctznode_weekly.py
+wsl.exe ~/IconProject/.analysis/bin/python ~/IconProject/wallet_ranking/block_differences.py
+pause
