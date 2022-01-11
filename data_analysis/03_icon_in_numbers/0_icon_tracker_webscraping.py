fb85035feffaf8a3998a4fba05b75bc6a79ce128
Tono || Sung <tonoplast.chloroplast@gmail.com>
2022-01-12 00:27:34 +1100

    Update 0_icon_tracker_webscraping.py

--- data_analysis/03_icon_in_numbers/0_icon_tracker_webscraping.py
+++ data_analysis/03_icon_in_numbers/0_icon_tracker_webscraping.py
@@ -21,6 +21,11 @@ import numpy as np
 import os
 from datetime import date, datetime, timedelta
 import glob
+from typing import Union
+from iconsdk.icon_service import IconService
+from iconsdk.providers.http_provider import HTTPProvider
+from iconsdk.builder.call_builder import CallBuilder
+from iconsdk.wallet.wallet import KeyWallet
 
 desired_width=320
 pd.set_option('display.width', desired_width)
@@ -33,9 +38,89 @@ inPath = os.path.join(currPath, "output")
 resultPath = os.path.join(inPath, "icon_tracker")
 if not os.path.exists(resultPath):
     os.mkdir(resultPath)
+
 dataPath = os.path.join(resultPath, "data")
 if not os.path.exists(dataPath):
     os.mkdir(dataPath)
+
+walletPath = os.path.join(currPath, "wallet")
+if not os.path.exists(walletPath):
+    os.mkdir(walletPath)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ wallet ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+## Creating Wallet if does not exist (only done for the first time)
+tester_wallet = os.path.join(walletPath, "test_keystore_1")
+
+if os.path.exists(tester_wallet):
+    wallet = KeyWallet.load(tester_wallet, "abcd1234*")
+else:
+    wallet = KeyWallet.create()
+    wallet.get_address()
+    wallet.get_private_key()
+    wallet.store(tester_wallet, "abcd1234*")
+
+tester_address = wallet.get_address()
+
+SYSTEM_ADDRESS = "cx0000000000000000000000000000000000000000"
+
+# using solidwallet
+icon_service = IconService(HTTPProvider("https://ctz.solidwallet.io/api/v3"))
+
+# loop to icx converter
+def loop_to_icx(loop):
+    icx = loop / 1000000000000000000
+    return(icx)
+
+def parse_icx(val: str) -> Union[float, int]:
+    """
+    Attempts to convert a string loop value into icx
+        Parameters:
+            val (str): Loop value
+        Returns:
+            (Union[float, int]): Will return the converted value as an int if successful, otherwise it will return NAN
+    """
+    try:
+        return loop_to_icx(int(val, 0))
+    except ZeroDivisionError:
+        return float("NAN")
+    except ValueError:
+        return float("NAN")
+
+def hex_to_int(val: str) -> Union[int, float]:
+    """
+    Attempts to convert a string based hex into int
+        Parameters:
+            val (str): Value in hex
+        Returns:
+            (Union[int, float]): Returns the value as an int if successful, otherwise a float NAN if not
+    """
+    try:
+        return int(val, 0)
+    except ValueError:
+        print(f"failed to convert {val} to int")
+        return float("NAN")
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ iiss info ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# get iiss info
+def get_iiss_info():
+    call = CallBuilder().from_(tester_address) \
+        .to(SYSTEM_ADDRESS) \
+        .method("getIISSInfo") \
+        .build()
+    result = icon_service.call(call)['variable']
+
+    df = {'Icps': hex_to_int(result['Icps']), 'Iglobal': parse_icx(result['Iglobal']),
+          'Iprep': hex_to_int(result['Iprep']),
+          'Irelay': hex_to_int(result['Irelay']), 'Ivoter': hex_to_int(result['Ivoter'])}
+
+    return df
+
+iglobal = get_iiss_info()['Iglobal']
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
 
 ## value extraction function
 def extract_values(obj, key):
@@ -82,6 +167,7 @@ def extract_json(json_dict):
 
 
 icx_df = extract_json(jtracker_url)
+icx_df['iglobal'] = iglobal
 
 # today date
 # today = date.today()
