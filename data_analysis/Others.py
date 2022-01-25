5210ebcaf7b93e1f56c7b533397a94f529e37d63
Tono || Sung <tonoplast.chloroplast@gmail.com>
2022-01-25 14:20:02 +1100

    Create Others.py

---
+++ data_analysis/Others.py
@@ -0,0 +1,9 @@
+
+
+
+# Pandas is used for data manipulation
+import pandas as pd
+# Read in data and display first 5 rows
+features = pd.read_csv('C:\\Users\\sungw\\Documents\\temps.csv')
+features.head(5)
+
