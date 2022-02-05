5bf84cb33b7f25760daf146ab30d5097525444ae
Tono || Sung <tonoplast.chloroplast@gmail.com>
2022-02-06 09:25:18 +1100

    Update daily_prep_votes_snapshot.bat

--- data_analysis/daily_prep_votes_snapshot.bat
+++ data_analysis/daily_prep_votes_snapshot.bat
@@ -1,4 +1,5 @@
 call python E:\GitHub\ICONProject\data_analysis\03_icon_in_numbers\0_daily_prep_votes_snapshot.py
+call python E:\GitHub\ICONProject\data_analysis\03_icon_in_numbers\0_daily_prep_votes_snapshot_ctz_node.py
 call python E:\GitHub\ICONProject\data_analysis\03_icon_in_numbers\0_icon_tracker_webscraping.py
 wsl.exe ~/IconProject/.analysis/bin/python ~/IconProject/wallet_ranking/exchange_wallet_data_extract_single_api.py
 
