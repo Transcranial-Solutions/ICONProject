4b106a6c264fbd638d3200ccb93f26776f010668
tonoplast <tonoplast.chloroplast@gmail.com>
2022-06-28 14:35:22 +1000

    updating cron

--- data_analysis/2_daily_snapshot_after_12_UTC.py
+++ data_analysis/2_daily_snapshot_after_12_UTC.py
@@ -12,8 +12,8 @@ token_transfer_path = '/home/tono/ICONProject/data_analysis/10_token_transfer'
 transaction_data_path = '/home/tono/ICONProject/data_analysis/08_transaction_data'
 
 
-program_list = [os.path.join(token_transfer_path, "token_transfer_ubuntu.py"),
-                os.path.join(transaction_data_path,"block_extraction_ubuntu.py"), 
+program_list = [os.path.join(token_transfer_path, "token_transfer_community_ubuntu.py"),
+                os.path.join(transaction_data_path,"block_extraction_community_ubuntu.py"), 
                 os.path.join(transaction_data_path,"transaction_extraction_ctznode_ubuntu.py"),
                 os.path.join(transaction_data_path,"transaction_analysis_ctznode_ubuntu.py"),
                 os.path.join(transaction_data_path,"transaction_analysis_ctznode_weekly_ubuntu.py"),
