e08fea1c73f675d734c86c28dc6dc8d2af397a40
tonoplast <tonoplast.chloroplast@gmail.com>
2022-10-14 00:30:05 +0000

    Automatic save commit initiated at Fri 14 Oct 2022 00:30:05 UTC

--- data_analysis/1_daily_snapshot_before_12_UTC.py
+++ data_analysis/1_daily_snapshot_before_12_UTC.py
@@ -15,7 +15,7 @@ wallet_ranking_path = '/home/tono/ICONProject/data_analysis/06_wallet_ranking'
 program_list = [os.path.join(daily_path, "0_icon_community_tracker_webscraping_ubuntu.py"),
                 os.path.join(daily_path,"0_daily_prep_votes_snapshot_ubuntu.py"), 
                 os.path.join(daily_path,"0_daily_prep_votes_snapshot_ctz_node_ubuntu.py"),
-                os.path.join(daily_path,"0_icon_tracker_webscraping_ubuntu.py"),
+                # os.path.join(daily_path,"0_icon_tracker_webscraping_ubuntu.py"),
                 os.path.join(wallet_ranking_path,"exchange_wallet_data_extract_single_api_ubuntu.py"),
                 ]
 
